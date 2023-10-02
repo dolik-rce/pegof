69bbdcc1d06b6a867bf4c4ff7163aa0dd315c1cd
Jan Dolinar <jan.dolinar@firma.seznam.cz>
2023-10-02 21:42:52 +0200

    fix directives formatting

--- ast/directive.cc
+++ ast/directive.cc
@@ -14,14 +14,14 @@ void Directive::parse(Parser& p) {
     if (p.match_re("%(earlysource|earlycommon|earlyheader|source|header|common)")) {
         name = p.last_re_match.str(1);
         if(p.match_code()) {
-            value = p.last_match;
+            value = trim(p.last_match);
         }
         code = true;
         valid = true;
     } else if (p.match_re("%(value|auxil|prefix)")) {
         name = p.last_re_match.str(1);
         if (p.match_quoted("\"", "\"") || p.match_quoted("'", "'")) {
-            value = p.last_match;
+            value = trim(p.last_match);
         }
         code = false;
         valid = true;
@@ -33,11 +33,31 @@ void Directive::parse(Parser& p) {
 
 std::string Directive::to_string() const {
     std::string result = format_comments() + "%" + name;
-    if (code) {
-        result += " {" + value + "}";
+    result += code ? " {"  : " \"";
+
+    if (code && value[0] == '#') {
+        // PackCC doesn't support single line C directives e.g.: %header { #include "x.h" }
+        result += "\n    " + value + "\n";
+    } else if (code) {
+        std::vector<std::string> lines = split(value, "\\n");
+        if (lines.size() > 1) {
+            // multiline code
+            result += "\n";
+            std::string last_line = lines.back();
+            for (int i = 0; i < last_line.size(); i++) {
+                // copy indent from last line to the first line of result
+                if (isspace(last_line[i])) result += last_line[i];
+                else break;
+            }
+            result += value + "\n";
+        } else {
+            // single line code
+            result += " " + value + " ";
+        }
     } else {
-        result += " \"" + value + "\"";
+        result += value;
     }
+    result += code ? "}"  : "\"";
     return result;
 }
 
