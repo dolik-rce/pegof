928332f6b697926a98becb94bf3c46a29968bc44
Jan Dolinar <jan.dolinar@firma.seznam.cz>
2024-08-11 16:54:10 +0200

    add analytics to github pages

--- web/glue.js
+++ web/glue.js
@@ -14,6 +14,14 @@ function log(...args) {
     }
 }
 
+function stats(path, title) {
+    window.goatcounter.count({
+        path:  path,
+        title: title,
+        event: true
+    });
+}
+
 function compress(c){var x="charCodeAt",b,e={},f=c.split(""),d=[],a=f[0],g=256;for(b=1;b<f.length;b++)c=f[b],null!=e[a+c]?a+=c:(d.push(1<a.length?e[a]:a[x](0)),e[a+c]=g,g++,a=c);d.push(1<a.length?e[a]:a[x](0));for(b=0;b<d.length;b++)d[b]=String.fromCharCode(d[b]);return d.join("")}
 
 function decompress(b){var a,e={},d=b.split(""),c=f=d[0],g=[c],h=o=256;for(b=1;b<d.length;b++)a=d[b].charCodeAt(0),a=h>a?d[b]:e[a]?e[a]:f+c,g.push(a),c=a.charAt(0),e[o]=f+c,o++,f=a;return g.join("")}
@@ -95,6 +103,7 @@ function launch() {
     runBtn.disabled = true;
     runBtn.innerText = "Running ...";
     serializeState();
+    stats("run", "Pegof was run");
     setTimeout(() => {
         Module.callMain(options);
         runBtn.disabled = false;
@@ -117,6 +126,7 @@ function loadPeg() {
             input.innerHTML = data;
             peg = data;
             log("Loaded", load.value, data.substr(0, 50));
+            stats("loaded-" + load.value, "Loaded " + load.value);
             launchAuto();
         });
 }
@@ -125,6 +135,7 @@ function init() {
     deserializeState();
 
     load.addEventListener("change", loadPeg);
+    auto.addEventListener("change", () => stats("auto-update-" + auto.checked, "Auto-update turned " + (auto.checked ? "on" : "off")));
 
     document.querySelectorAll(".opt").forEach(x => x.addEventListener("change", launchAuto));
     runBtn.addEventListener("click", launch);
